79150f5a3d67688da7128c0e489d731062616a70
Uhmar Vince Aceron <aceronuhmarvince@gmail.com>
2024-12-17 15:52:06 +0800

    dd

--- secondScript.js
+++ secondScript.js
@@ -12,7 +12,17 @@ window.addEventListener("load", function () {
         }
     }, 500); 
 }); 
-  
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav");
+
+    hamburger.addEventListener("click", () => {
+        const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
+        hamburger.setAttribute("aria-expanded", !expanded);
+        navMenu.classList.toggle("show");
+    });
+});
 
 
